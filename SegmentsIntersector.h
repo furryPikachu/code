500e7d28b5ed1bd6b69f82a286f4026e47325c60
Linjie Ying <3150102074@zju.edu.cn>
2020-01-01 13:56:36 +0800

    add head file: SegmentsIntersector.

---
+++ SegmentsIntersector.h
@@ -0,0 +1,16 @@
+#ifndef SEGMENTSINTERSECTOR_H
+#define SEGMENTSINTERSECTOR_H
+
+#include "Segment.H"
+#include <map>
+#include <set>
+
+/* Compute the intersections of a multiset of line segments */
+
+class SegmentsIntersector
+{
+  
+  
+ }
+
+#endif
